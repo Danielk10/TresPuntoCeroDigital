fcfafead9b6260c460e5388ad6cf75b09c164763
Danielk10 <danielpdiamon@gmail.com>
2024-10-12 01:55:14 -0400

    update

---
+++ app/src/main/java/com/trespuntocerodigital/actividades/GaleriaActivity.java
@@ -0,0 +1,30 @@
+package com.diamon.actividades;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import com.diamon.trespuntocerodigital.Galeria;
+
+public class GaleriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Crear un FrameLayout programáticamente
+        FrameLayout frameLayout = new FrameLayout(this);
+        frameLayout.setId(FrameLayout.generateViewId()); // Generar un ID para el FrameLayout
+        setContentView(frameLayout);
+
+        // Verifica si el savedInstanceState es nulo para evitar la recreación del fragmento
+        if (savedInstanceState == null) {
+            // Crea el fragmento de equipo
+            Galeria equipoFragment = new Galeria(this, "Nuestro Equipo");
+
+            // Agrega el fragmento al contenedor (FrameLayout)
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().add(frameLayout.getId(), equipoFragment).commit();
+        }
+    }
+}
